4eef580d4e2764d28c110eb3cf683c06ebfff5dd
seoyoon park <seoyoon@seoyoons-MacBook-Pro.local>
2017-11-19 00:39:51 +1100

    object created

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+  {
+   rank: "queen",
+   suit: "hearts",
+   cardImage:"images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds"
+  }
+];
 var cardsInPlay = [];
 var checkForMath = function() {
   if(cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,8 +29,10 @@ var checkForMath = function() {
   }
 }
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId])
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank)
+  console.log("User flipped " + cards[cardId].cardImage)
+  console.log("User flipped " + cards[cardId].suit)
+  cardsInPlay.push(cards[cardId].rank);
 
   /*var cardOne = cards[0];
   var cardTwo = cards[2];
