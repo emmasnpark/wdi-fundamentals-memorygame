360492da57464b114fbd9c5cd4b4aba5dd37440a
seoyoon park <seoyoon@seoyoons-MacBook-Pro.local>
2017-11-18 15:03:40 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+var card = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = card[0];
+var cardTwo = card[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay[0] === cardOne) {
+  console.log("User flipped queen.")
+}
+if(cardsInPlay[1] === cardTwo) {
+    console.log("User flipped king.")
+}
+if(cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again");
+}
