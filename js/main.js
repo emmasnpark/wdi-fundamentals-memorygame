cea39fddf07b7aa0139720ce7000b6b6ac06965e
seoyoon park <seoyoon@seoyoons-MacBook-Pro.local>
2017-11-18 16:18:34 +1100

    created game function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-var card = ["queen", "queen", "king", "king"];
+var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
-var cardOne = card[0];
-var cardTwo = card[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-if (cardsInPlay[0] === cardOne) {
-  console.log("User flipped queen.")
+var checkForMath = function() {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again");
+  }
 }
-if(cardsInPlay[1] === cardTwo) {
-    console.log("User flipped king.")
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId])
+  cardsInPlay.push(cards[cardId]);
+
+  /*var cardOne = cards[0];
+  var cardTwo = cards[2];
+  cardsInPlay.push(cardOne);
+  cardsInPlay.push(cardTwo);
+  if (cardsInPlay[0] === cardOne) {
+    console.log("User flipped queen.")
+  }
+  if(cardsInPlay[1] === cardTwo) {
+      console.log("User flipped king.")
+  }
+  */
+  checkForMath();
 }
-if(cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again");
-}
+flipCard(0);
+flipCard(2);
