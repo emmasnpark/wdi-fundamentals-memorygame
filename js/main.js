d0d7a1c66cbb56d2e9ba686ab0e613c0fbe21b07
seoyoon park <seoyoon@seoyoons-MacBook-Pro.local>
2017-11-20 02:06:38 +1100

    final version

--- js/main.js
+++ js/main.js
@@ -7,45 +7,42 @@ var cards = [
   {
     rank: "queen",
     suit: "diamonds",
-    cardImage: "images/queen-of-diamonds"
+    cardImage: "images/queen-of-diamonds.png"
   },
   {
     rank: "king",
     suit: "hearts",
-    cardImage: "images/king-of-hearts"
+    cardImage: "images/king-of-hearts.png"
   },
   {
     rank: "king",
     suit: "diamonds",
-    cardImage: "images/king-of-diamonds"
+    cardImage: "images/king-of-diamonds.png"
   }
 ];
 var cardsInPlay = [];
 var checkForMath = function() {
-  if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again");
-  }
+  if(cardsInPlay[1]) {
+      if(cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+      } else {
+        alert("Sorry, try again");
+      }
+   } return
 }
-var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId].rank)
-  console.log("User flipped " + cards[cardId].cardImage)
-  console.log("User flipped " + cards[cardId].suit)
+var flipCard = function() {
+  cardId = this.getAttribute('data-id');
+  this.setAttribute('src', cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
-
-  /*var cardOne = cards[0];
-  var cardTwo = cards[2];
-  cardsInPlay.push(cardOne);
-  cardsInPlay.push(cardTwo);
-  if (cardsInPlay[0] === cardOne) {
-    console.log("User flipped queen.")
-  }
-  if(cardsInPlay[1] === cardTwo) {
-      console.log("User flipped king.")
-  }
-  */
   checkForMath();
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+  for(var i = 0; i < cards.length; i++) {
+     var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement)[i];
+     }
+}
+createBoard();
